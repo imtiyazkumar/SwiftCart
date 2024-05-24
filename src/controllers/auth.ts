cfdb21604be79771573cb617a6ccd76b8636e6f3
Imtiyaz Ahmad <imtiyazah907@gmail.com>
2024-05-25 00:15:17 +0530

    code formatting

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { prismaClient } from './../index';
 import { hashSync, compareSync } from 'bcrypt'
 import * as jwt from "jsonwebtoken";
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
 import express, { Request, Response } from "express";
-import { PORT } from "./secrets";
-import rootRouter from "./routes";
 import { PrismaClient } from "@prisma/client";
 import { errorMiddleware } from "./middlewares/errors";
-import { SignUpSchema } from "./schema/users";
+import { PORT } from "./secrets";
+import rootRouter from "./routes";
 
 const app = express();
 
-
 app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => res.send("Welcome to SwiftCart! Server is up and working"))
 
 app.use("/api", rootRouter)
 
 app.use(errorMiddleware)
 
-export const prismaClient = new PrismaClient({
-    log: ["query"]
-})
+export const prismaClient = new PrismaClient({ log: ["query"] })
 
 app.listen(PORT, () => console.log("Server running at http://localhost:8090/"));
